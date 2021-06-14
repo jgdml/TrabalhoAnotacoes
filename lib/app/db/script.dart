c194c3a122a4751b890ba13467396ae50e2c35a5
jgdml <47838646+jgdml@users.noreply.github.com>
2021-06-14 15:46:08 -0300

    script update

--- lib/app/db/script.dart
+++ lib/app/db/script.dart
@@ -1,15 +1,14 @@
-
-final createAnotacao = """
+final createTables = """
 CREATE TABLE ANOTACAO(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    TITULO VARCHAR(40) NOT NULL
+    TITULO VARCHAR(40) NOT NULL,
     TEXTO VARCHAR(1000),
   	
-  	DT_CRIACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  	DT_CRIACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    DT_MODIFICACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-""";
 
-final createImagem = """"
+
 CREATE TABLE IMAGEM(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     ID_ANOTACAO INTEGER NOT NULL,
@@ -21,18 +20,20 @@ CREATE TABLE IMAGEM(
 """;
 
 
-final insert1 = """
-INSERT INTO ANOTACAO(TEXTO)
-VALUES ('Descrição da anotação número 1');
+
+final insertsTeste = """
+INSERT INTO ANOTACAO(TITULO, TEXTO)
+VALUES ('Titulo 1', 'Descrição da anotação número 1');
 
 INSERT INTO IMAGEM (ID_ANOTACAO, URL)
 VALUES(1, 'https://media.istockphoto.com/photos/abstract-orange-background-picture-id679762938');
-""";
 
-final insert2 = """
-INSERT INTO ANOTACAO(TEXTO)
-VALUES ('Descição da anotação número 2');
+
+
+INSERT INTO ANOTACAO(TITULO, TEXTO)
+VALUES ('Titulo 2', 'Descição da anotação número 2');
 
 INSERT INTO IMAGEM (ID_ANOTACAO, URL)
 VALUES(2, 'https://media.istockphoto.com/photos/abstract-orange-background-picture-id679762938');
 """;
+
