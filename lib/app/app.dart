396da2fb70fdce14ae1277abb33cd6959c824ba7
jgdml <47838646+jgdml@users.noreply.github.com>
2021-06-15 15:38:00 -0300

    mudança title

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
-        title: 'Editor',
+        title: 'Anotações',
         theme: ThemeData(
 
             primarySwatch: Colors.indigo,
