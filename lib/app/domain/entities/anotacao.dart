cd279ed098512eb5f8c4b94f10506dc8b49968eb
jgdml <47838646+jgdml@users.noreply.github.com>
2021-09-30 19:57:05 -0300

    Conversao json

--- lib/app/domain/entities/anotacao.dart
+++ lib/app/domain/entities/anotacao.dart
@@ -1,4 +1,4 @@
-class Anotacao{
+class Anotacao {
     dynamic id;
 
     String? titulo;
@@ -6,4 +6,22 @@ class Anotacao{
     String? dtModificacao;
 
     Anotacao({this.id, this.titulo, this.texto, this.dtModificacao});
+
+    static Map<String, dynamic> toJson(Anotacao anotacao) {
+        return {
+            "id": anotacao.id,
+            "titulo": anotacao.titulo,
+            "texto": anotacao.texto,
+            "dtModificacao": anotacao.dtModificacao,
+        };
+    }
+
+    static Anotacao fromJson(Map<String, dynamic> json) {
+        return Anotacao(
+            id: json['id'],
+            titulo: json['titulo'],
+            texto: json['texto'],
+            dtModificacao: json['dtModificacao'],
+        );
+    }
 }
